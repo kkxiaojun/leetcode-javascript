cc6b95a886d3510cbe00c6bed3d9e2a3070bfc59
zhaoguojun <985531883@qq.com>
2019-12-05 22:31:17 +0800

    完成第一题

--- sort/sort.js
+++ sort/sort.js
@@ -42,3 +42,18 @@ console.log(intersect([1,2,2,1], [2,2]))
 
 console.log(intersect([4,9,5], [9,4,9,8,4]))
 
+
+// 解法二：暴力法
+// 遍历数组长度小的数组
+// 遍历第一个数组，然后在第二个数组查找是否有当前元素。
+// 如果有，把当前元素push进返回值。然后把第二个数组里该下标元素删除。
+var intersect1 = function(nums1, nums2) {
+  let result = [];
+  if (nums1.length < nums2.length) [nums1, nums2] = [nums2, nums1];
+  for (let i = 0; i < nums1.length; i++) {
+    let key = nums2.indexOf(nums1[i]);
+    if (key !== -1) result.push(nums2.splice(key, 1));
+  }
+  return result;
+};
+
