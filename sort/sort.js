3f003d4a211432460ef3b7276609115a09ad9b3a
zhaoguojun <985531883@qq.com>
2020-04-28 17:03:37 +0800

    tree

--- sort/sort.js
+++ sort/sort.js
@@ -113,3 +113,100 @@ var findNumberIn2DArray = function(matrix, target) {
   }
   return false
 };
+
+var preorderTraversal = function(root) {
+  var printArr = []
+  var stack = []
+  stack.push(root)
+  while(stack.length > 0) {
+    var popValue = stack.pop()
+    printArr.push(popValue.val)
+    popValue.right && stack.push(popValue.right)
+    popValue.left && stack.push(popValue.left)
+  }
+  return printArr
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+  if (root) {
+    return [[root.val], levelOrder(root.left), levelOrder(root.right)]
+  } else {
+    return []
+  }
+};
+
+console.log('levelOrder:', )
+
+
+var levelOrder = function(root) {
+  if(!root)
+    return []
+  var levels = [root]
+  var result = []
+  while(levels.length > 0){
+    var n = levels.length
+    // 每一层的迭代结果
+    var currentResult = []
+    // 每一层的迭代
+    while( n-- > 0){
+      // 弹出第一项
+      var node = levels.shift()
+      currentResult.push(node.val)
+      node.left && levels.push(node.left)
+      node.right && levels.push(node.right)
+    }
+    result.push(currentResult)
+  }
+  return result
+};
+
+// 层次遍历：采用队列的方式
+var levelOrder1 = function(root) {
+  if (!root) {
+    return []
+  }
+  var levels = [root]
+  var result = []
+  while (levels.length > 0) {
+    var n = levels.length
+    var currentResult = []
+    while (n-- > 0) {
+      var node = levels.shift()
+      currentResult.push(node.val)
+      node.left && levels.push(node.left)
+      node.right && levels.push(node.right)
+    }
+    result.push(currentResult)
+  }
+  return result
+};
+
+// 先序遍历：采用栈的方式
+var preorderTraversal = function(root) {
+  if (!root) {
+    return []
+  }
+  var stack = []
+  var resultArr = []
+  stack.push(root)
+  while (stack.length > 0) {
+    var popValue = stack.pop()
+    resultArr.push(popValue.val)
+    popValue.right && stack.push(popValue.right)
+    popValue.left && stack.push(popValue.left)
+  }
+  return resultArr
+}
+
