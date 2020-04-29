4e0e01c32a7b6013e877e3fd092373875409da2c
guojun <985531883@qq.com>
2020-04-29 21:52:16 +0800

    3

--- sort/sort.js
+++ sort/sort.js
@@ -113,3 +113,36 @@ var findNumberIn2DArray = function(matrix, target) {
   }
   return false
 };
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+  var p1 = m -1
+  var p2 = n -1
+  var p = m + n -1
+  while ((p1 >= 0) && (p2 >= 0)) {
+    nums1[p--] = nums1[p1] < nums2[p2] ? nums2[p2--] : nums1[p1--]
+  }
+  // add missing elements from nums2
+  nums1.splice(0, p2 + 1, ...(nums2.slice(0, p2 + 1)))
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+
+};
