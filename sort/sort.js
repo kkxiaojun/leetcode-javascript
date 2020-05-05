7f6e1f769da617d3d5287a078e896fdc109b4298
guojun <985531883@qq.com>
2020-05-05 23:30:21 +0800

    二叉树

--- sort/sort.js
+++ sort/sort.js
@@ -6,39 +6,18 @@
  * }
  */
 /**
- * @param {TreeNode} root
- * @return {number}
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
  */
-var maxDepth = function(root) {
-  if (!root) {
-    return 0
+var buildTree = function(preorder, inorder) {
+  if (!preorder.length) {
+    return null
   }
-  var leftDepth = maxDepth(root.left)
-  var rightDepth =maxDepth(root.right)
-  return Math.max(leftDepth, rightDepth) + 1
-};
-
-console.log('root:', maxDepth([3,9,20,null,null,15,7]))
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} sum
- * @return {boolean}
- */
-var hasPathSum = function(root, sum) {
-  if (!root) {
-    return false
-  }
-  sum -= root.val
-  if (!root.left && !root.right) {
-    return sum === 0
-  }
-  hasPathSum(root.left, sum) || hasPathSum(root.right, sum)
+  var tmp = preorder[0]
+  var mid = inorder.indexOf(tmp)
+  var root = new TreeNode(tmp)
+  root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid))
+  root.right = buildTree(preorder.slice(mid + 2), inorder.slice(mid + 1, inorder.length - 1))
+  return root
 };
