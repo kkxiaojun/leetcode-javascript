0a028a7e0f72d7a8c9d37d0e79f7356edb2eb6fc
zhaoguojun <985531883@qq.com>
2020-05-18 20:55:10 +0800

    add: 数组总结1

--- sort/sort.js
+++ sort/sort.js
@@ -7,17 +7,51 @@
  */
 /**
  * @param {TreeNode} root
- * @return {boolean}
+ * @param {number} val
+ * @return {TreeNode}
  */
-const helper = (root, lower, upper) => {
+var searchBST = function(root, val) {
   if (!root) {
-    return true
+    return null
   }
-  if (root.val <= lower || root.val >= upper) {
-    return false
+  if (root.val === val) {
+    return root
   }
-  return helper(root.left, lower, root.val) && helper(root.right, root.val, upper)
-}
-var isValidBST = function(root) {
-  return helper(root, -Infinity, Infinity)
+  return (root.left && searchBST(root.left, val)) || (root.right && searchBST(root.right, val))
 };
+
+
+
+// 反转字符串 
+var reverseString = function(s) {
+  if (s.length == 0) {
+    return s
+  }
+  var i = 0
+  var j = s.length - 1
+  while(i < j) {
+    var item = s[i]
+    s[i] = s[j]
+    s[j] = item
+    i++
+    j--
+  }
+  return s
+};
+
+// 两数之和: target - nums[1] 存在于map中
+var twoSum = function(nums, target) {
+  if (nums.length === 0) {
+    return nums
+  }
+  var mapObj = new Map()
+  // 先储存一份数据
+  for (let nums = 0; nums < nums.length; nums++) {
+    Map.set(nums[i], nums[i])
+  }
+  // target - nums[1]
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+  }
+}
